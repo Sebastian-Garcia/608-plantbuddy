cc85110bebbaf3eabe41bbacbd88b5791117fa4e
Timmy Dang <timmyd@mit.edu>
2022-05-09 23:59:58 -0400

    Added preset list

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ import json
 import smtplib
 
 
-plant_db = '/var/jail/home/team58/plantbuddy/plants.db' 
-plant_reading_db = '/var/jail/home/team58/plantbuddy/plants_reading.db' 
-plant_sampling_db = '/var/jail/home/team58/plantbuddy/plants_sampling.db' 
+plant_db = '/var/jail/home/team58/plantbuddy/plant.db' 
+plant_reading_db = '/var/jail/home/team58/plantbuddy/plant_reading.db' 
+plant_sampling_db = '/var/jail/home/team58/plantbuddy/plant_sampling.db' 
 
 def send_notification(message, recipient="sebastianag2002@gmail.com"):
 
@@ -38,95 +38,112 @@ def request_handler(request):
     soil_reading = 'no readings yet'
 
     if request['method'] =="GET":
-
         try:
-            if len(request['values'].keys()) == 0 :
+                plantName = request["values"]["name"]
+                user = request["values"]["user"]
                 with sqlite3.connect(plant_db) as c:
-                    c.execute("""CREATE TABLE IF NOT EXISTS plant_data (plant text, sunlight real, temperature real, moisture real );""")
-                    things = c.execute('''SELECT * FROM plant_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-                with sqlite3.connect(plant_reading_db) as c:
-                    c.execute("""CREATE TABLE IF NOT EXISTS plant_reading_data (sunlight_reading real, temperature_reading real, moisture_reading real);""")
-                    readings = c.execute('''SELECT * FROM plant_reading_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-                outs = {"plant": things[0][0], "sunlight": things[0][1], "temperature": things[0][2], "moisture": things[0][3]}
-                reading_outs = {"sunlight_reading": readings[0][0], "temperature_reading": readings[0][1], "moisture_reading": readings[0][2]}
-                plant = outs['plant']
-                sunlight = outs['sunlight']
-                temperature = outs['temperature']
-                moisture = outs['moisture']
-                sunlight_reading = reading_outs['sunlight_reading']
-                temp_reading = reading_outs['temperature_reading']
-                soil_reading = reading_outs['moisture_reading']
-                return htmlString.format(n=plant, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading)
-            plant = request["values"]["plant"]
-            with sqlite3.connect(plant_db) as c:
-                c.execute("""CREATE TABLE IF NOT EXISTS plant_data (plant text, sunlight real, temperature real, moisture real );""")
-                things = c.execute('''SELECT * FROM plant_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-            with sqlite3.connect(plant_reading_db) as c:
-                c.execute("""CREATE TABLE IF NOT EXISTS plant_reading_data (sunlight_reading real, temperature_reading real, moisture_reading real);""")
-                readings = c.execute('''SELECT * FROM plant_reading_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-            outs = {"plant": things[0][0], "sunlight": things[0][1], "temperature": things[0][2], "moisture": things[0][3]}
-            reading_outs = {"sunlight_reading": readings[0][0], "temperature_reading": readings[0][1], "moisture_reading": readings[0][2]}
-            plant = outs['plant']
-            sunlight = outs['sunlight']
-            temperature = outs['temperature']
-            moisture = outs['moisture']
-            sunlight_reading = reading_outs['sunlight_reading']
-            temp_reading = reading_outs['temperature_reading']
-            soil_reading = reading_outs['moisture_reading']
-            return htmlString.format(n=plant, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading)
-        except:
-            return "error or plant doesn't exist"
-        
-        
+                    c.execute("""CREATE TABLE IF NOT EXISTS plant_data (plant text, type text, user text, sunlight real, temperature real, moisture real);""")
+                    things = c.execute('''SELECT * FROM plant_data WHERE plant = ? AND user = ? ORDER BY rowid DESC LIMIT 1;''', (plantName, user)).fetchall()
+                    plantType = things[0][1]
+                    sunlight = things[0][3]
+                    temperature = things[0][4]
+                    moisture = things[0][5]
 
-        # if "esp32" in request['values']:
-        #     sunlight = c.execute('''SELECT sunlight FROM plant_data WHERE plant = ?;''',(plant,)).fetchall()
-        #     moisture = c.execute('''SELECT moisture FROM plant_data  WHERE plant = ?;''',(plant,)).fetchall()
-        #     return (sunlight[0][0], moisture[0][0])
+                try:
+                    with sqlite3.connect(plant_reading_db) as c:
+                        c.execute("""CREATE TABLE IF NOT EXISTS plant_reading_data (plant text, user text, sunlight_reading real, temperature_reading real, moisture_reading real);""")
+                        readings = c.execute('''SELECT * FROM plant_reading_data WHERE plant = ? AND user = ? ORDER BY rowid DESC LIMIT 1;''', (plantName, user)).fetchall()
+                        sunlight_reading = readings[0][0]
+                        temp_reading = readings[0][1]
+                        soil_reading = readings[0][1]
+                except:
+                    pass
+
+
+                return htmlString.format(n=plantName, pt= plantType, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading)
+        except:
+            return htmlString.format(n=plantName, pt= plantType, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading)
+
         
     elif request['method'] =="POST":
-        # if 'form' in request.keys() and 'from' not in request["form"]:
-        #     user = request['form']['owner']
-        #     plant = request['form']['name']
-        #     sunlight = request['form']['light']
-        #     temperature = request['form']['temp']
-        #     moisture = request['form']['soil']
-        #     with sqlite3.connect(plant_db) as c:
-        #         c.execute("""CREATE TABLE IF NOT EXISTS plant_data (plant text, sunlight real, temperature real, moisture real);""")
-        #         c.execute('''INSERT into plant_data VALUES (?,?,?,?);''',(plant,sunlight,temperature, moisture))
-        #     with sqlite3.connect(plant_reading_db) as c:
-        #         readings = c.execute('''SELECT * FROM plant_reading_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-        #     reading_outs = {"sunlight_reading": readings[0][0], "temperature_reading": readings[0][1], "moisture_reading": readings[0][2]}
-        #     sunlight_reading = reading_outs['sunlight_reading']
-        #     temp_reading = reading_outs['temperature_reading']
-        #     soil_reading = reading_outs['moisture_reading']
-        #     return htmlString.format(n=plant, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading) 
-        # elif 'form' in request.keys() and 'from' in request["form"] and 'arduino' == request["form"]['from']:
-        #     info = request["form"]
-        #     sunlight_reading = float(info["sunlight"])
-        #     temp_reading = float(info["temp"])
-        #     soil_reading = float(info["moisture"])
-        #     with sqlite3.connect(plant_db) as c:
-        #         # sunlight = c.execute('''SELECT sunlight FROM plant_data WHERE plant = ? ORDER BY rowid DESC LIMIT 1;''',(plant,)).fetchall()
-        #         # moisture = c.execute('''SELECT moisture FROM plant_data  WHERE plant = ? ORDER BY rowid DESC LIMIT 1;''',(plant,)).fetchall()
-        #         # temperature = c.execute('''SELECT temperature FROM plant_data  WHERE plant = ? ORDER BY rowid DESC LIMIT 1;''',(plant,)).fetchall()
-        #         #c.execute('''INSERT into plant_data VALUES (?,?,?,?);''',(plant,sunlight,temperature, moisture))
-        #         things = c.execute('''SELECT * FROM plant_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-        #         c.execute("""CREATE TABLE IF NOT EXISTS plant_reading_data (sunlight_reading real, temperature_reading real, moisture_reading real);""")
-        #     with sqlite3.connect(plant_reading_db) as c:
-        #         c.execute('''INSERT into plant_reading_data VALUES (?,?,?);''',(sunlight_reading,temp_reading,soil_reading))
-        #         readings = c.execute('''SELECT * FROM plant_reading_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
-        #     outs = {"plant": things[0][0], "sunlight": things[0][1], "temperature": things[0][2], "moisture": things[0][3]}
-        #     plant = outs['plant']
-        #     sunlight = outs['sunlight']
-        #     temperature = outs['temperature']
-        #     moisture = outs['moisture']
+        try:
+            if request['args'] == []:
+                with sqlite3.connect(plant_sampling_db) as c:
+                    c.execute("""CREATE TABLE IF NOT EXISTS plant_sampling_data (plant text, user text, counter int);""")
+                    c.execute('''INSERT into plant_sampling_data VALUES (?,?,?);''',(plantName,user,0))
 
-        #     return htmlString.format(n=plant, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading)
+            if 'form' in request.keys() and 'from' not in request["form"]:
+                user = request['form']['user']
+                plantName = request['form']['name']
+                plantType = request['form']['type']
+
+                if plantType == 'Snake Plant':
+                    sunlight = 10
+                    temperature = 10
+                    moisture = 10
+                elif plantType == 'Peace Lily':
+                    sunlight = 20
+                    temperature = 20
+                    moisture = 20
+                elif plantType == 'Fiddle Leaf Fig':
+                    sunlight = 30
+                    temperature = 30
+                    moisture = 30
+                elif plantType == 'Philodendron':
+                    sunlight = 40
+                    temperature = 40
+                    moisture = 40
+                elif plantType == 'ZZ Plant':
+                    sunlight = 50
+                    temperature = 50
+                    moisture = 50
+                elif plantType == 'Pothos':
+                    sunlight = 60
+                    temperature = 60
+                    moisture = 60
+                elif plantType == 'Majesty Palm':
+                    sunlight = 70
+                    temperature = 70
+                    moisture = 70
+                elif plantType == 'Aloe':
+                    sunlight = 80
+                    temperature = 80
+                    moisture = 80
+                else:
+                    moisture = request['form']['soil']
+                    sunlight = request['form']['light']
+                    temperature = request['form']['temp']
+                # add new plant to database
+                with sqlite3.connect(plant_db) as c:
+                    c.execute("""CREATE TABLE IF NOT EXISTS plant_data (plant text, type text, user text, sunlight real, temperature real, moisture real);""")
+                    c.execute('''INSERT into plant_data VALUES (?,?,?,?,?,?);''',(plantName,plantType,user,sunlight,temperature,moisture))
+                # start a GET counter for new sampling device
+                with sqlite3.connect(plant_sampling_db) as c:
+                    c.execute("""CREATE TABLE IF NOT EXISTS plant_sampling_data (plant text, user text, counter int);""")
+                    c.execute('''INSERT into plant_sampling_data VALUES (?,?,?);''',(plantName,user,0))
+
+                return htmlString.format(n=plantName, pt=plantType, o=user, s=sunlight, t=temperature, m=moisture, sr=sunlight_reading, tr=temp_reading,mr=soil_reading) 
+
+            # handle arduino post request
+            elif 'form' in request.keys() and 'from' in request["form"] and 'arduino' == request["form"]['from']:
+                info = request["form"]
+                plantName = info["name"]
+                user = info["user"]
+                sunlight_reading = float(info["sunlight"])
+                temp_reading = float(info["temperature"])
+                soil_reading = float(info["moisture"])
+                with sqlite3.connect(plant_reading_db) as c:
+                    c.execute("""CREATE TABLE IF NOT EXISTS plant_reading_data (plant text, user text, sunlight_reading real, temperature_reading real, moisture_reading real);""")
+                    c.execute('''INSERT into plant_reading_data VALUES (?,?,?,?,?);''',(plantName,user,sunlight_reading,temp_reading,soil_reading))
+                    # readings = c.execute('''SELECT * FROM plant_reading_data ORDER BY rowid DESC LIMIT 1;''').fetchall()
+                return
 
         
         
         
-        
-    # else:
-        return "invalid HTTP method for this url."
+        except:
+            return "invalid HTTP method for this url."
+
+
+def do_plant_logic():
+    pass
